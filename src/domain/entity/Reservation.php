ebe602f439e53ed673e026208685773cc2042b77
Hugodrc55 <hugo.darocha77@gmail.com>
2025-12-08 11:36:23 +0100

    modif resa

--- src/domain/entity/Reservation.php
+++ src/domain/entity/Reservation.php
@@ -10,21 +10,12 @@ class Reservation
     
     private DateTimeImmutable $finReservation;
 
-    /**
-     * Constructeur simplifié pour les tests
-     * 
-     * @param DateTimeImmutable $fin Date de fin de la réservation
-     */
+  
     public function __construct(DateTimeImmutable $fin)
     {
         $this->finReservation = $fin;
     }
 
-    /**
-     * Retourne la date de fin de la réservation
-     * 
-     * @return DateTimeImmutable
-     */
     public function getFinReservation(): DateTimeImmutable
     {
         return $this->finReservation;
